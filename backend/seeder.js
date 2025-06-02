d3013a7c9f2a1279c7841e8f1f8259d120c6a2f0
NguyenTruong354 <nguyenhuutruong.96pch@gmail.com>
2025-06-02 14:32:38 +0700

    Refactor seeder.js to replace ES6 imports with CommonJS require statements for consistency

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import users from './data/users.js'
-import products from './data/products.js'
-import User from './models/userModel.js'
-import Product from './models/productModel.js'
-import Order from './models/order.model.js'
-import Category from './models/category.model.js'
-import connectDB from './config/db.js'
-import categories from './data/category.js'
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const users = require('./data/users');
+const products = require('./data/products');
+const User = require('./models/userModel');
+const Product = require('./models/productModel');
+const Order = require('./models/order.model');
+const Category = require('./models/category.model');
+const connectDB = require('./config/db');
+const categories = require('./data/category');
 
 dotenv.config()
 
