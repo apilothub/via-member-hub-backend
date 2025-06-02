fb839d68c10172e7268d3a6533338dd47395a594
NguyenTruong354 <nguyenhuutruong.96pch@gmail.com>
2025-06-02 14:33:10 +0700

    Fix import path for challengeController in challengeRoutes.js

--- backend/routes/challengeRoutes.js
+++ backend/routes/challengeRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const challengeController = require('../controller/challengeController');
+const challengeController = require('../controllers/challengeController');
 
 // Middleware: Lấy userId từ query string và gán vào req.user cho controller dùng
 router.use((req, res, next) => {
