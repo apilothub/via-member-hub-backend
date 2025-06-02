9fc47fc0d16f085fbb4fd6da69a9cbe16c7edb1d
khacdat0511 <khac.dat0511@gmail.com>
2025-06-02 18:08:34 +0700

    add token router

---
+++ backend/routes/tokenRouter.js
@@ -0,0 +1,5 @@
+const tokenController = require('../controllers/token.controller');
+const express = require('express');
+const router = express.Router();
+const middleware=require('../middleware/auth.middleware');
+router.get('/gettoken',middleware.checkTokenFE(),tokenController.getToken);
