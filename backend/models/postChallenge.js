c41019275fdc428e1c896af95937ebc04f9ca1bb
Huy <hghfghf147@gmail.com>
2025-06-05 21:04:53 +0700

    update postChallenge model

--- backend/models/postChallenge.js
+++ backend/models/postChallenge.js
@@ -2,31 +2,31 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const PostChallenge = sequelize.define('post_challenges', {
-  id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true },
-  type: { 
-    type: DataTypes.ENUM('comment', 'event'), 
-    allowNull: false 
+  post_challenge_id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true },
+  type: {
+    type: DataTypes.ENUM('comment', 'event'),
+    allowNull: false
   },
   promo_id: { type: DataTypes.BIGINT },
   challenge_id: { type: DataTypes.BIGINT },
   post_id: { type: DataTypes.BIGINT },
   event_id: { type: DataTypes.BIGINT },
-  is_show: { 
-    type: DataTypes.BOOLEAN, 
-    defaultValue: false 
+  is_show: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
   },
-  points_reward: { 
-    type: DataTypes.INTEGER, 
-    defaultValue: 0 
+  points_reward: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
   },
   due_date: { type: DataTypes.DATE },
-  created_at: { 
-    type: DataTypes.DATE, 
-    allowNull: false 
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false
   },
-  updated_at: { 
-    type: DataTypes.DATE, 
-    allowNull: false 
+  updated_at: {
+    type: DataTypes.DATE,
+    allowNull: false
   }
 }, {
   tableName: 'post_challenges',
