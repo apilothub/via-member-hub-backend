8f67e2bf4a3cdf30febfba604267be2219ce84f7
khacdat0511 <khac.dat0511@gmail.com>
2025-06-06 11:04:26 +0700

    Update token.controller.js
    Signed-off-by: khacdat0511 <khac.dat0511@gmail.com>

--- backend/controllers/token.controller.js
+++ backend/controllers/token.controller.js
@@ -2,7 +2,7 @@ const auth = require('../utils/authToken.js');
 const userController=require('../controllers/user.controller.js')
 const getToken = async (req, res) => {
     try {
-        let decode = req.user
+        let decode = req.data
         let token = auth.generateAccessToken(decode)
         res.setHeader('Authorization', `Bearer ${token}`);
         return res.status(200).json({message:"Access Token successfully"});
