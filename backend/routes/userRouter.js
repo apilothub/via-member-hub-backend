d8c93690bc86ec2fceca9cabdb1db7d8e1a34415
khacdat0511 <khac.dat0511@gmail.com>
2025-06-06 16:06:11 +0700

    delete middle for test

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -2,7 +2,7 @@ const userController = require('../controllers/user.controller');
 const express = require('express');
 const router = express.Router();
 const middleware=require('../middleware/auth.middleware');
-router.get('/alluser',middleware.checkToken() ,userController.getUsers);
-router.get('/userbyid',middleware.checkToken() ,userController.getUsersById);
+router.get('/alluser',userController.getUsers);
+router.get('/userbyid',userController.getUsersById);
 
 module.exports = router;
