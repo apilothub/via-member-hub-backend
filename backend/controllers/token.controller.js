90c2b35e731503f4b180d9088e96d87d77b0b7fa
khacdat0511 <khac.dat0511@gmail.com>
2025-06-06 11:41:07 +0700

    update token controller

--- backend/controllers/token.controller.js
+++ backend/controllers/token.controller.js
@@ -1,11 +1,13 @@
 const auth = require('../utils/authToken.js');
 const userController=require('../controllers/user.controller.js')
+const dotenv=require('dotenv')
+dotenv.config()
 const getToken = async (req, res) => {
     try {
-        let user = await userController.getUsers
-        let token = auth.generateAccessToken(user)
+        let secretKey = process.env.SECRET_KEY
+        let token = auth.generateAccessToken(secretKey)
         res.setHeader('Authorization', `Bearer ${token}`);
-        return res.status(200).json({message:"Access Token successfully"});
+        return res.status(200).json({message:"Token sent successfullyAccess Token successfully"});
     } catch (err) {
         res.status(500).json({message: "Error getting token"});
     }
