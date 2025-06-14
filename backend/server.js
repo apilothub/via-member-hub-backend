2b8e99185b43db2e3fe353b008c8205454dcc723
khacdat0511 <khac.dat0511@gmail.com>
2025-06-14 20:32:22 +0700

    add middleware

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const challengeRoutes = require('./routes/challengeRoutes');
 const userRouter = require('./routes/userRouter');
 const eventRouter = require('./routes/event.routes.js');
 const tokenRouter=require('./routes/tokenRouter.js')
+const authMiddleware=require('./middleware/auth.middleware.js')
 const {notFound, errorHandler} = require('./middleware/error.middleware')
 
 
@@ -21,9 +22,9 @@ const port = process.env.PORT || 5000;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.use('/api/challenges', challengeRoutes);
-app.use('/api/users', userRouter);
+app.use('/api/users',authMiddleware.checkToken(), userRouter);
 app.use('/api/events', eventRouter);
-app.use('/api/token', tokenRouter);
+app.use('/api/token',authMiddleware.checkTokenFE(),tokenRouter);
 
 
 app.use(notFound)
