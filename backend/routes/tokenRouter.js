31da1893db439898dd2a459a089b1622279584a8
khacdat0511 <khac.dat0511@gmail.com>
2025-06-14 20:30:24 +0700

    remove middleware

--- backend/routes/tokenRouter.js
+++ backend/routes/tokenRouter.js
@@ -1,6 +1,5 @@
 const tokenController = require('../controllers/token.controller');
 const express = require('express');
 const router = express.Router();
-const middleware=require('../middleware/auth.middleware');
-router.get('/gettoken',middleware.checkTokenFE(),tokenController.getToken);
+router.get('/gettoken',tokenController.getToken);
 module.exports=router
