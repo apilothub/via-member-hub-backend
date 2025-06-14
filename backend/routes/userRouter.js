76798b56ff783f2eb56406add10629e4f17c22c4
khacdat0511 <khac.dat0511@gmail.com>
2025-06-14 20:25:23 +0700

    remove middleware

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,7 @@
 const userController = require('../controllers/user.controller');
 const express = require('express');
 const router = express.Router();
-const middleware=require('../middleware/auth.middleware');
-router.get('/alluser',middleware.checkToken(),userController.getUsers);
-router.get('/:id',middleware.checkToken(),userController.getUsersById);
+router.get('/alluser',userController.getUsers);
+router.get('/:id',userController.getUsersById);
 
 module.exports = router;
